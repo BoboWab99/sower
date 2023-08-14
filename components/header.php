fc98be7b96acc93caf9d328762a173fa89c74064
BoboWab99 <wingabire.arnaud@strathmore.edu>
2023-08-14 17:17:24 +0300

    header made component

---
+++ components/header.php
@@ -0,0 +1,82 @@
+<?php
+
+// start session if it isn't started
+if (session_id() == '' || !isset($_SESSION) || session_status() === PHP_SESSION_NONE) {
+   session_start();
+
+   // is user logged in?
+   $is_user_logged_in = isset($_SESSION['auth_user']);
+
+   if ($is_user_logged_in) {
+      $user = $_SESSION['auth_user'];
+   }
+}
+
+?>
+
+
+<header class="header navbar navbar-expand-lg sticky-top bg-white shadow--1" id="header">
+   <nav class="container bd-gutter flex-wrap flex-lg-nowrap" aria-label="Main navigation">
+      <a class="navbar-brand" href="<?php echo $path_prefix . "index.php" ?>">Sower</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item">
+               <a class="nav-link active" href="<?php echo $path_prefix . "index.php" ?>">Home</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link active" href="<?php echo $path_prefix . "pages/posts/all.php" ?>">Posts</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
+               <a class="nav-link" href="#">Another link...</a>
+            </li> -->
+         </ul>
+
+         <form class="d-flex mx-auto" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-primary" type="submit">Search</button>
+         </form>
+
+         <div class="ms-lg-3 d-inline-flex gap-2">
+            <?php
+            if ($is_user_logged_in) {  // @if is_user_logged_in
+            ?>
+               <div class="dropdown">
+                  <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     <?php echo "@" . $user->username; ?>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-end">
+                     <li>
+                        <div class="px-4 pt-1 pb-2 text-center">
+                           <h6 class="mb-0 text-capitalize"><?php echo $user->full_name; ?></h6>
+                           <a class="d-block" href="mailto:<?php echo $user->email; ?>"><?php echo $user->email; ?></a>
+                           <span class="d-block"><?php echo "@" . $user->username; ?></span>
+                        </div>
+                     </li>
+                     <li>
+                        <hr class="dropdown-divider">
+                     </li>
+                     <li><a class="dropdown-item" href="#">My Posts</a></li>
+                     <li><a class="dropdown-item" href="#">Profile</a></li>
+                     <li>
+                        <hr class="dropdown-divider">
+                     </li>
+                     <li><a class="dropdown-item" href="<?php echo $path_prefix . "logic/logout.php" ?>">Logout</a></li>
+                  </ul>
+               </div>
+
+            <?php
+            } else { // @else is_user_logged_in
+            ?>
+               <a href="<?php echo $path_prefix . "pages/core/register.php" ?>" role="button" class="btn btn-primary">Register</a>
+               <a href="<?php echo $path_prefix . "pages/core/login.php" ?>" role="button" class="btn btn-outline-primary">Login</a>
+            <?php
+            } // @endif is_user_logged_in
+            ?>
+         </div>
+      </div>
+   </nav>
+</header>
--- pages/core/register.php
+++ pages/core/register.php
@@ -36,12 +36,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    if (mysqli_query($conn, $query)) {
       header('Location: login.php');
       exit();
-      
    } else {
       echo "Error: " . $query . "<br>" . mysqli_error($conn);
    }
    mysqli_close($conn);
 }
+
+
+// path prefix
+$path_prefix = "../../";
 
 // --- END LOGIC ---
 ?>
@@ -54,8 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Register</title>
-   <?php include_once "../../components/bootstrap-css.php" ?>
-   <link rel="stylesheet" href="../../css/app.css">
+   <?php include_once $path_prefix . "components/css-links.php" ?>
 </head>
 
 <body>
@@ -63,86 +65,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    <!-- https://stackoverflow.com/questions/18022809/how-to-solve-error-mysql-shutdown-unexpectedly -->
    <!-- https://stackoverflow.com/questions/768431/how-do-i-make-a-redirect-in-php -->
 
-   <header class="header navbar navbar-expand-lg sticky-top bg-white shadow--1" id="header">
-      <nav class="container bd-gutter flex-wrap flex-lg-nowrap" aria-label="Main navigation">
-         <a class="navbar-brand" href="../../">Sower</a>
-         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-         </button>
-
-         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-               <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-               </li>
-               <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                     Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                     <li><a class="dropdown-item" href="#">Action</a></li>
-                     <li><a class="dropdown-item" href="#">Another action</a></li>
-                     <li>
-                        <hr class="dropdown-divider">
-                     </li>
-                     <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-               </li>
-            </ul>
-
-            <form class="d-flex" role="search">
-               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-               <button class="btn btn-outline-primary" type="submit">Search</button>
-            </form>
-
-            <div class="ms-lg-3">
-               <?php if ($is_user_logged_in) { ?>
-
-                  <div class="dropdown">
-                     <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <?php echo "@" . $user->username; ?>
-                     </a>
-                     <ul class="dropdown-menu dropdown-menu-end">
-                        <li>
-                           <div class="px-4 pt-1 pb-2 text-center">
-                              <h6 class="mb-0 text-capitalize"><?php echo $user->full_name; ?></h6>
-                              <a class="d-block" href="mailto:<?php echo $user->email; ?>"><?php echo $user->email; ?></a>
-                              <span class="d-block"><?php echo "@" . $user->username; ?></span>
-                           </div>
-                        </li>
-                        <li>
-                           <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">My Posts</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li>
-                           <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="logic/logout.php">Logout</a></li>
-                     </ul>
-                  </div>
-
-               <?php
-               } else { // @else is_user_logged_in
-               ?>
-                  <a href="login.php" role="button" class="btn btn-primary">Login</a>
-
-               <?php
-               } // @endif is_user_logged_in
-               ?>
-            </div>
-         </div>
-      </nav>
-   </header>
+   <?php include_once $path_prefix . "components/header.php" ?>
 
    <div class="py-5">
       <div class="col-10 col-md-7 col-lg-5 col-xl-4 mx-auto">
          <h4 class="mb-4">Register a new account</h4>
 
-         <form class="d-grid gap-3" action="../../logic/register.php" method="POST" id="form">
+         <form class="d-grid gap-3" method="POST" id="form">
             <div class="form-field">
                <label for="username">Username:</label>
                <input class="form-control" type="text" name="username" id="username" pattern="[a-zA-Z0-9_]{4,}" placeholder="Enter Your Username" required autofocus>
@@ -188,7 +117,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    </div>
 
 
-   <?php include_once "../../components/bootstrap-js.php" ?>
+   <?php include_once $path_prefix . "components/js-links.php" ?>
 
    <script>
       // some form validation
